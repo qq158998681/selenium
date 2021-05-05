4ec03159a295612c69fe2c8b73de20e8d37726b2
qq158998681 <liujie1663@163.com>
2021-05-05 17:02:55 +0800

    修复bug

--- api/baidu_main.py
+++ api/baidu_main.py
@@ -1,15 +1,38 @@
 from pagebase import selenium_public
 from pagebase import runlog
+from selenium.common.exceptions import NoSuchElementException
 
 
 class Main(selenium_public.Base):
     url = "http://www.baidu.com"
 
-    def open_baidu(self):
-        self.id("kw").send_keys("123")
-        self.id("su").click()
-        a = self.css(".c-abstract > em").text
-        return a
+    def open_baidu3(self):
+        try:
+            self.id("kw").send_keys("123")
+            self.id("su").click()
+            a = self.css(".c-abstract > em").text
+            return a
+        except NoSuchElementException:
+            self.log.logError("没有定位到这个元素")
+            return False
+        except Exception:
+            self.log.logError("未知异常")
+            return False
+
+    def open_baidu2(self):
+        try:
+            self.id("k").send_keys("123")
+            self.id("su").click()
+            a = self.css(".c-abstract > em").text
+            return a
+        except NoSuchElementException:
+            self.log.logError("没有定位到这个元素")
+            return False
+        except Exception:
+            self.log.logError("未知异常")
+            return False
+
+
 
 if __name__ == '__main__':
     a = Main()
--- testcase/test_baidu.py
+++ testcase/test_baidu.py
@@ -1,7 +1,7 @@
 from pagebase import runlog
 import unittest
 from api import baidu_main
-from selenium.common.exceptions import NoSuchElementException
+
 
 class TestBaidu(unittest.TestCase):
 
@@ -9,24 +9,22 @@ class TestBaidu(unittest.TestCase):
         self.log = runlog.RunLog()
 
     def tearDown(self):
-        pass
+        a = baidu_main.Main().quit()
+
+    def test_open_baidu2(self):
+        text = baidu_main.Main().open_baidu2()
+        self.assertEqual(text, "456", "pass")
+        self.log.logInfo("测试用例001通过")
 
     def test_open_baidu(self):
-        text = baidu_main.Main().open_baidu()
-        try:
-            self.assertEqual(text, "123", "pass")
-            self.log.logInfo("测试用例001通过")
-        except NoSuchElementException :
-            self.log.logError("没有找到这个元素,信息")
-        except AssertionError :
-            self.log.logWarning("预期元素与实际不符，预期{}，实际{}".format("123", text))
-        except Exception :
-            self.log.logError("未知异常")
+        text = baidu_main.Main().open_baidu3()
+        self.assertEqual(text, "123", "pass")
+        self.log.logInfo("测试用例001通过")
 
 
 
-
-
+if __name__ == '__main__':
+    unittest.main()
 
 
 
